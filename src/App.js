57cde2d41e1d459b9cb9304c2cd3cf4a6e1dd1bb
Prabesh Humagain <Humagain.prabesh@outlook.com>
2020-12-16 20:42:45 -0600

    removed material ui dependedncies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/App.css';
-// import createMixins from '@material-ui/core/styles/createMixins';
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      date:new Date(),
-      numbers:[1,2,3,4,5,6],
-    };
-  }
-  componentDidMount(){
-    this.timerId = setInterval(() => {
-      this.setState({
-        date:new Date()
-      })
-    }, 100);
-  }
-  componentWillUnmount(){
-    clearInterval(this.timerId);
-  }
-   
-  render(){
+
+class App extends Component {
+  state = {  }
+  render() { 
     return (
-      <div>
-        <h1>Site under construction</h1>
-      </div>
-    )
+      <React.Fragment>
+        Toolbar page
+      </React.Fragment>
+     );
   }
 }
  
